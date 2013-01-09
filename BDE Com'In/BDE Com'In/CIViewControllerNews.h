bf1f53bd8bef2494ab559a4a51274a4f5a92433a
Mehdi Kacimi <kacimi.mehdi@gmail.com>
2013-01-09 18:00:08 +0100

    Arborescence

---
+++ BDE Com'In/BDE Com'In/CIViewControllerNews.h
@@ -0,0 +1,13 @@
+//
+//  CIViewControllerNews.h
+//  BDE Com'In
+//
+//  Created by Mehdi on 09/01/13.
+//  Copyright (c) 2013 Ingesup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CIViewControllerNews : UIViewController
+
+@end
---
+++ BDE Com'In/BDE Com'In/CIViewControllerNews.m
@@ -0,0 +1,38 @@
+//
+//  CIViewControllerNews.m
+//  BDE Com'In
+//
+//  Created by Mehdi on 09/01/13.
+//  Copyright (c) 2013 Ingesup. All rights reserved.
+//
+
+#import "CIViewControllerNews.h"
+
+@interface CIViewControllerNews ()
+
+@end
+
+@implementation CIViewControllerNews
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
