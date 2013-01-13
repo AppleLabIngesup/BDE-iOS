13cd41919494a632eae553509d40de098505da7e
Milliere Christohe <christophe.milliere@ingesup.com>
2013-01-13 02:26:55 +0100

    Création du menu avec viewdeck plus création d'une cellule custom pour le menu

---
+++ BDE Com'In/BDE Com'In/CICellViewLeft.h
@@ -0,0 +1,13 @@
+//
+//  CICellViewLeft.h
+//  BDE Com'In
+//
+//  Created by chrism on 13/01/13.
+//  Copyright (c) 2013 Ingesup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CICellViewLeft : UITableViewCell
+
+@end
---
+++ BDE Com'In/BDE Com'In/CICellViewLeft.m
@@ -0,0 +1,29 @@
+//
+//  CICellViewLeft.m
+//  BDE Com'In
+//
+//  Created by chrism on 13/01/13.
+//  Copyright (c) 2013 Ingesup. All rights reserved.
+//
+
+#import "CICellViewLeft.h"
+
+@implementation CICellViewLeft
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
