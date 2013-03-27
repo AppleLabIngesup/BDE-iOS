6af8dcf354027bef148920324f8762841666ceca
Hourlier <gaby.hourlier@ingesup.com>
2013-03-27 12:13:54 +0100

    Section partenaire

---
+++ BDE Com'In/BDE Com'In/CICellPartner.h
@@ -0,0 +1,13 @@
+//
+//  CICellPartner.h
+//  BDE Com'In
+//
+//  Created by Hellsingblack on 20/02/13.
+//  Copyright (c) 2013 Ingesup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CICellPartner : UIViewController
+
+@end
